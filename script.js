cd770ad1c30e92bdb9653b20668c72732a142391
nanaGitH <lfebryano@gmail.com>
2024-01-23 16:33:02 +0700

    collision blocks

--- script.js
+++ script.js
@@ -1,66 +1,49 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
 
 const scaledCanvas = {
     width: canvas.width / 4,
-    height: canvas.height /4
+    height: canvas.height / 4
 }
-
-const c = canvas.getContext('2d');
 
 const gravity = 0.5;
 
-class Sprite
-{
-    constructor({position, imageSrc}) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-    }
-
-    draw() {
-        if(!this.image){
-            return
-        }
-        c.drawImage(this.image, this.position.x, this.position.y);
-    }
-
-    update() {
-        this.draw();
-    }
+const floorCollisions2D = [];
+const platformCollision2D = [];
+for(let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36));
+    platformCollision2D.push(platformCollisions.slice(i, i + 36));
 }
 
-class Player
-{
-    constructor(position) {
-        this.position = position,
-        this.velocity = {
-            x: 0,
-            y: 1
-        },
-        this.height = 100
-    }
+const collisionBlocks = [];
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol === 1008)
+        collisionBlocks.push(new CollisionBlock({
+    position: {
+        x: x * 16,
+        y: y * 16}
+    }));
+});
+});
+const platformCollisionBlocks = [];
+platformCollision2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol === 1008)
+        platformCollisionBlocks.push(new CollisionBlock({
+            position: {
+                x: x * 16,
+                y: y * 16}
+        }));
+    });
+});
 
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 100, this.height);
-    }
 
-    update() {
-        this.draw();
+console.log(platformCollision2D);
 
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        
-        if( this.position.y + this.height + this.velocity.y < canvas.height){
-            this.velocity.y += gravity;
-        }else {
-            this.velocity.y = 0;
-        }
-    }
-}
 
 const player = new Player({x:500,y:100});
 const player2 = new Player({x:100,y:0});
@@ -85,7 +68,10 @@ function animate () {
     c.scale(4, 4); //tidak merubah dimensi original image
     c.translate(0, -background.image.height + scaledCanvas.height)
     background.update();
+    collisionBlocks.forEach(e => e.update())
+    platformCollisionBlocks.forEach(e => e.update())
     c.restore();
+
 
     player.update();
     player2.update();
